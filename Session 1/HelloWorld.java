e04e15bf6af1c4d8143802de01bf8e2c753246cd
LukeZerafaOffical <107716056+LukeZerafaOffical@users.noreply.github.com>
2022-09-24 17:55:03 +0200

    Hello_World uploaded

---
+++ Session 1/HelloWorld.java
@@ -0,0 +1,11 @@
+//Task: It prints hello screen to the terminal.
+//Written by:
+//Date: 24/09/2022
+
+class HelloWorld //Create a new class called HelloWorld, must be the same name of the file.
+{
+    public static void main(String [] args)//Thw main method that tells the program to start here, also called an entry point
+    {
+        System.out.println("Hello, World"); //Code that prints onto the terminal
+    }
+}
